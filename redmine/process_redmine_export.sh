82b961e75c7072c4bc8fa14f0c6ba94ee0c0079d
Lubos Kocman <Lubos.Kocman@suse.com>
2020-02-11 14:23:05 +0100

    Initial import of openSUSE Leap 15.2 release steps
    * Import was done as part of Hackweek19
      Input was redmine.redmine-export-openSUSE-Leap-15.2-20200205.csv
      data transformation done by md2workflow/bin/redmine2md commit 3a8c42c3
      available in https://github.com/openSUSE/md2workflow (branch hackweek19)
    
    * Tasks for Max and Lubos were replaced by groups as defined in
      project_config.md. Substition of remaining task owners TBD
    
    * Resolves github.com/openSUSE/openSUSE-release-process Issue #1

--- redmine/process_redmine_export.sh
+++ redmine/process_redmine_export.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-set -x
+echo "Running this script will overwrite content of ../*.md"
+echo "Press Enter to continue or Ctrl+C to abort"
+read
 
 for target_version in Alpha Beta RC GM GA; do
-    redmine2md redmine-export-*.csv --target-version ${target_version} >  ${target_version}.md
-    dos2unix ${target_version}.md
+    echo "Processing ../${target_version}.md"
+    redmine2md redmine-export-*.csv --target-version ${target_version} >  ../${target_version}.md
+    dos2unix ../${target_version}.md
 done
 
-# Test if we skipped some
-redmine2md redmine-export-*.csv --target-version ${target_version} >  ${target_version}.md
+# Substitute users with groups defined in [ownership] ../openSUSE-Leap.conf
+
+echo "Current task owners"
+grep --no-filename "Responsible:" ../*.md | sort | uniq
+# output as of Feb 11th 2020. TODO: Confirm with others on default assignment
+#Responsible: AdaLovelace
+#Responsible: hellcp
+#Responsible: jsegitz
+#Responsible: lkocman
+echo "Substituting user lkocman"; sed -i "s/Responsible:\ lkocman/Responsible:\ rel-mgmt/"  ../*.md
+#Responsible: mgriessmeier
+#Responsible: mlin7442
+echo "Substituting user mlin7442"; sed -i "s/Responsible:\ mlin7442/Responsible:\ rel-eng/"  ../*.md
